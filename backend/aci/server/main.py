44af3c0acca53523939da7b574c0fb681372ed55
SahilShetLYZR <sahil@lyzr.ai>
2025-09-23 11:23:29 +0530

    ADDED LOCAL

--- backend/aci/server/main.py
+++ backend/aci/server/main.py
@@ -138,7 +138,7 @@ app.add_middleware(SessionMiddleware, secret_key=config.SIGNING_KEY)
 # )
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["*"],
+    allow_origins=["http://localhost:3000"],
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
