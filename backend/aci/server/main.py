98d7c1bce7fc3728f490c95ea48fd969a53e6b39
SahilShetLYZR <sahil@lyzr.ai>
2025-10-09 12:41:45 +0530

    Chore: Modified CORS

--- backend/aci/server/main.py
+++ backend/aci/server/main.py
@@ -132,6 +132,7 @@ app.add_middleware(
     allow_origins=[
         "http://localhost:3000",
         "https://main.de73cci4api6i.amplifyapp.com",
+        "https://aci.studio.lyzr.ai",
     ],
     allow_credentials=True,
     allow_methods=["*"],
